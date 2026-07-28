dd7c440a632e0e68e5e1d3fd2ff565841a70eeb4
Tushar Dhiman <dhimantushar2007@gmail.com>
2026-07-28 20:26:59 +0530

    api  updated

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -1,6 +1,6 @@
   import React, { useState, useEffect, useContext } from "react";
   import { useParams, useNavigate } from "react-router-dom";
-  import api from "../utils/axios";
+  import api from "../utils/axios.js";
   import { AuthContext } from "../context/AuthContext";
   import {
     FaCalendarAlt,
