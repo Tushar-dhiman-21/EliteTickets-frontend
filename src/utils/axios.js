38ffaa1ae8a6404d6a5611247db6bcafe5c650ab
Tushar Dhiman <dhimantushar2007@gmail.com>
2026-07-28 16:58:59 +0530

    axios updated

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://elitetickets-backend.onrender.com/api"
+    baseURL: 'http://localhost:6900/api',
 });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
-
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
-
     return config;
 });
 
