990e08d270e4a571379308302870ecdd1af1f0dd
Tushar Dhiman <dhimantushar2007@gmail.com>
2026-07-29 18:50:33 +0530

    add

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://elitetickets-backend.onrender.com"
+    baseURL: "https://elite-tickets-backend.vercel.app/"
 });
 
 api.interceptors.request.use((config) => {
