f02aacf51e6abe11d4db1cda3b0208070400ef5d
Tushar Dhiman <dhimantushar2007@gmail.com>
2026-07-30 19:10:15 +0530

    add

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:5173"
+  baseURL: "http://localhost:6900/api",
 });
+
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
