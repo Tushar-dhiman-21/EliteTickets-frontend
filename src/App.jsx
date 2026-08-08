7fc3e6a57d88648c60f72083793fe78f1e0829f7
Tushar Dhiman <dhimantushar2007@gmail.com>
2026-08-08 12:32:54 +0530

    Layout Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,94 @@
 import React from "react";
 import Layout from "./components/Layout"
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import Home from "./pages/Home";
+import EventDetails from "./pages/EventDetails";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PaymentFailed from "./pages/PaymentFailed";
+import AdminDashboard from "./pages/AdminDashboard";
+import PaymentSuccess from "./pages/PaymentSucess";
+import UserDashboard from "./pages/UserDashboard";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import Payment from "./pages/Payment";
 
 function App() {
+    const routes=createBrowserRouter([{
+        path:"/",
+        element:<Layout/>,
+        children:[
+            
+            {
+            path: "/home",
+        element: <Home />,
+            },
+            {
+                path:"/events/:id",
+                 element:<EventDetails />
+            },
+            {
+                path:"/login",
+                 element:<Login />
+            
+            },
+            {
+                path:"/register",
+                 element:< Register/>
+            
+            },
+            {
+                path:"/dashboard",
+                 element:< UserDashboard/>
+            
+            }
+            ,
+            {
+                path:"/admin",
+                 element:< AdminDashboard/>
+            
+            },
+            {
+                path:"/payment-success",
+                 element:< PaymentSuccess/>
+            
+            },
+            {
+                path:"/payment-failed",
+                 element:< PaymentFailed/>
+            
+            },
+            {
+                path:"/forgot-password",
+                 element:<ForgotPassword/>
+            
+            },
+            {
+                path:"/reset-password",
+                 element:<ResetPassword/>
+            
+            },
+            {
+                path:"/payment/:id",
+                 element:<Payment/>
+            
+            },
+            {
+                path:"*",
+                element:<h1 className="text-3xl font-bold text-center mt-20">404 - Page Not Found</h1>
+            }
+    
+    ]
+
+
+    }]
+)
+
     return (
-        <Layout/>
-           );
+        
+         <><RouterProvider router={routes}></RouterProvider>            
+       </>
+    );
 }
 
 export default App;
